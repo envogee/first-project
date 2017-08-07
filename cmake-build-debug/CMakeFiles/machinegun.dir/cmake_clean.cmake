c1c0812fc40c064b31a33e3311fba2914817a805
envogee <corneliussaiki@gmail.com>
2017-08-07 08:36:23 -0500

    first update

---
+++ cmake-build-debug/CMakeFiles/machinegun.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/machinegun.dir/main.cpp.o"
+  "machinegun.pdb"
+  "machinegun"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/machinegun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int ad (int x ,int y)
+{
+return (x +y);
+}
+//version one
+int main() {
+    int a=80;
+    int b=90;
+    int c=ad(a,b);
+    
+    std::cout <<c << std::endl;
+    return 0;
+}
